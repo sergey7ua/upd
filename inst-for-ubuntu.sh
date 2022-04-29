5bf45512b87cb2f1038e77a74dd335fc126272b3
sergey7ua <50703599+sergey7ua@users.noreply.github.com>
2022-04-29 23:40:02 +0300

    Update inst-for-ubuntu.sh

--- inst-for-ubuntu.sh
+++ inst-for-ubuntu.sh
@@ -1,26 +1,11 @@
 #!/bin/sh
 sudo apt update #&& sudo apt upgrade -y
-sudo curl -s https://raw.githubusercontent.com/SlavaUkraineSince1991/DDoS-for-all/main/scripts/docker_install.sh #| bash
+sudo curl -s https://raw.githubusercontent.com/SlavaUkraineSince1991/DDoS-for-all/main/scripts/docker_install.sh | bash
 sudo apt install mc -y 
 sudo apt install putty -y 
-
-#sudo apt install xfce4 -y
-#sudo apt install ubuntu-mate-desktop -y
 sudo apt-get install --no-install-recommends ubuntu-desktop
-
-#sudo apt install tasksel -y 
-#sudo tasksel install ubuntu-mate-desktop
-##"OR"
-#sudo tasksel install ubuntu-mate-core
-## -îñíîâíîé (äóìàþ ìèíèìàëêà)
-
 sudo apt install xrdp -y 
-#sudo systemctl status xrdp 
 sudo adduser xrdp ssl-cert
-#sudo systemctl restart xrdp
-#sudo useradd -m rdpuser
-#echo rdpuser:vubuntu | chpasswd
-
 USER=rdpuser
 PASS=vubuntu
 useradd -m $USER && $(echo $USER:$PASS | chpasswd)
